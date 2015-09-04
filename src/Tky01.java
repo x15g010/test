c6fef402acff3a95211ba24d200940455b93e0dd
unknown <x15g010@PC505N009.chiba-fjb.ac.jp>
2015-09-04 12:01:20 +0900

    修正

--- src/Tky01.java
+++ src/Tky01.java
@@ -2,8 +2,9 @@
 public class Tky01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		// TODO 自動生成されたメソッド・スタ
+		System.out.println("石岡大");
+		System.out.println("石岡大");
 	}
 
 }
